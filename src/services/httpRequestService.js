3544592639ddac82f8070acbbd3231480cb1c6a2
pavankumar upadhyayula <pavankumarupadhyayula@gmail.com>
2020-04-16 21:29:10 -0500

    Update httpRequestService.js

--- src/services/httpRequestService.js
+++ src/services/httpRequestService.js
@@ -2,7 +2,7 @@ const baseUrl = 'https://blockchain.info';
 const doAsyncRequest = async function RequestBlockChainData (path) {
     if(path !== undefined || path !== null) {
         const url = `${baseUrl}/${path}`;
-        const response = await fetch(url, { crossDomain: true, method: 'GET', cache: 'force-cache', headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*',mode:'no-cors'}});
+        const response = await fetch(url, { crossDomain: true, method: 'GET', cache: 'force-cache', headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': 'master.d2uuk4w19u6avx.amplifyapp.com', 'Access-Control-Allow-Headers':'Origin, X-Requested-With, Content-Type, Accept',mode:'no-cors'}});
         const data = await response.json();
         return data;
     }else{
