f5b05a45a301307158dc58ffa7b6b923b64661da
pavankumar upadhyayula <pavankumarupadhyayula@gmail.com>
2020-04-16 21:21:48 -0500

    code good

--- src/services/httpRequestService.js
+++ src/services/httpRequestService.js
@@ -2,7 +2,7 @@ const baseUrl = 'https://blockchain.info';
 const doAsyncRequest = async function RequestBlockChainData (path) {
     if(path !== undefined || path !== null) {
         const url = `${baseUrl}/${path}`;
-        const response = await fetch(url, { method: 'GET', cache: 'force-cache', headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'}});
+        const response = await fetch(url, { crossDomain: true, method: 'GET', cache: 'force-cache', headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*',mode:'no-cors'}});
         const data = await response.json();
         return data;
     }else{
