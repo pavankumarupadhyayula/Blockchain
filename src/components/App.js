f2976d5ab82aa7c2963c1177dd21df800a2a75fe
pavankumarupadhyayula <pavankumarupadhyayula@gmail.com>
2020-04-16 22:26:40 -0500

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,12 @@
 import React from "react";
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Dashboard from "./dashboard/Dashboard";
 import Blockinfo from "./modules/blockinfo/Blockinfo"
 
-// This site has 3 pages, all of which are rendered
-// dynamically in the browser (not server rendered).
-//
-// Although the page does not ever refresh, notice how
-// React Router keeps the URL up to date as you navigate
-// through the site. This preserves the browser history,
-// making sure things like the back button and bookmarks
-// work properly.
-
-export default function App() {
+function App() {
     return (
         <Router>
             <div>
-
                 <Switch>
                     <Route exact path="/">
                         <Dashboard />
@@ -32,4 +18,5 @@ export default function App() {
     );
 }
 
+export default App;
 
